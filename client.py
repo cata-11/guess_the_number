c399ed8a9f84fb6a62eff1cccb71df7ae0368328
cata-11 <catalin.11.munteanu@gmail.com>
2022-12-13 21:54:32 +0200

    player vs computer

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ PORT = 5001
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
 def connect_player():
     client_socket.connect((HOST, PORT))
 
@@ -14,38 +15,85 @@ def connect_player():
 
         if(message == 'Player not foound. The game will start between you and the computer...'):
             start_game_between_player_and_computer()    
-            break
 
-def get_input():
+
+def try_parse_int(value):
+    try:
+        return (int(value), True)
+    except ValueError:
+        return (value, False)
+
+
+def get_guess_input():
     while True:
         input_number = input("Your guess: ")
+        result = try_parse_int(input_number)
 
-        if(isinstance(int(input_number), int) and int(input_number) >= 1 and int(input_number) <= 50):
-            return input_number
+        if(result[1] and result[0] > 0 and result[0] < 51):
+            return result[0]
         else:
             print("You must enter a number between 1 and 50")
 
 
-def start_game_between_player_and_computer():
+def get_answer_input():
+    while True:
+        input_answer = input("Your answer: ")
 
+        if(input_answer.upper() == 'Y' or input_answer.upper() == 'N'):
+            return input_answer
+        else:
+            print("You must enter Y or N")
+
+
+def send_message(client_socket, message):
+    client_socket.send(message.encode('utf-8'))
+
+
+def handle_guess_message():
+    input_number = get_guess_input()
+    send_message(client_socket, str(input_number))
+
+    message = client_socket.recv(1024).decode('utf-8')
+    return message
+
+
+def handle_confirm_answer():
+
+    while True:
+        message = get_answer_input().upper()
+
+        if(message == 'Y'):
+            send_message(client_socket, message)
+            start_game_between_player_and_computer()
+
+        elif(message == 'N'):
+            send_message(client_socket, message)
+            message = client_socket.recv(1024).decode('utf-8')
+            print(message)
+            handle_player_exit()
+    
+        else:
+            print("You must enter Y or N")
+
+
+
+def handle_player_exit():
+    client_socket.close()
+    print('You have exited the game.')
+    exit(0)
+
+def start_game_between_player_and_computer():
     while True:
         message = client_socket.recv(1024).decode('utf-8')
         print(message)
 
-        if(message == 'Computer has chosen a number. Try to guess it...'):
+        if(message == 'Computer is choosing a number between 1 and 50...\nComputer has chosen a number. Try to guess it...'):
             while True:
-                input_number = get_input()
-                client_socket.send(input_number.encode('utf-8'))
-                message = client_socket.recv(1024).decode('utf-8')
+                message = handle_guess_message()
                 print(message)
 
                 if(message == 'You have guessed the number.\nDo you want to play again? (Y/N)'):
-                    input_answer = input("Your answer: ")
-                    client_socket.send(input_answer.encode('utf-8'))
+                    handle_confirm_answer()
 
-                    if(input_answer == 'Y'):
-                        start_game_between_player_and_computer()
-                    else:
-                        break
-           
+
 connect_player()
--- server.py
+++ server.py
@@ -12,20 +12,29 @@ server_socket.listen(2)
 clients_sockets = []
 clients_addresses = []
 
+player_vs_computer_game_scores = []
+
+
 def register_player(socket, address):
     clients_sockets.append(socket)
     clients_addresses.append(address)
     send_message(socket, 'You are connected')
     send_message(socket, 'Waiting for any other player to connect...')
 
+
 def send_message_to_all(message):
     for client_socket in clients_sockets:
         send_message(client_socket, message)
 
-def send_message(client_socket, message) :
+
+def send_message(client_socket, message):
     client_socket.send(message.encode('utf-8'))
 
+
 def connect_players():
+    print("Waiting for players to connect...")
+    server_socket.settimeout(None)
+
     while True:    
         client_socket, client_address = server_socket.accept()
 
@@ -49,36 +58,87 @@ def connect_players():
             send_message(clients_sockets[0], 'Player not foound. The game will start between you and the computer...')
 
             start_game_between_player_and_computer()
-            break
 
-def send_score():
-    print('Sending score...')
+
+def send_score(client_socket):
+    best_score = min(player_vs_computer_game_scores)
+    send_message(client_socket, 'Your best score is: ' + str(best_score))
+
+
+def try_parse_int(value):
+    try:
+        return (int(value), True)
+    except ValueError:
+        return (value, False)
+
+
+def handle_confirm_answer(message, client_socket, nr_of_tries):
+    while True:
+        if(message.upper() == 'Y'):
+            player_vs_computer_game_scores.append(nr_of_tries)
+            start_game_between_player_and_computer()
+
+        elif(message.upper() == 'N'):
+            player_vs_computer_game_scores.append(nr_of_tries)
+            send_score(client_socket)
+            handle_player_exit(client_socket)
+            connect_players()
+        else:
+            send_message(client_socket, 'You must enter Y or N')
+
+
+def handle_message(message, client_socket):
+    while True:
+        if(message.upper() == 'EXIT'):
+            handle_player_exit(client_socket)
+
+        result = try_parse_int(message)
+
+        if(result[1]):
+            return handle_guess_message(result[0], client_socket)
+
+        else:
+            send_message(client_socket, 'You must enter a number between 1 and 50')
+            message = client_socket.recv(1024).decode('utf-8')
+
+
+
+def handle_player_exit(client_socket):
+    client_socket.close()
+    print('Player has exited the game. Server is waiting for new connections...')
+    clients_sockets.remove(client_socket)
+    player_vs_computer_game_scores.clear()
+
+
+def handle_guess_message(number, client_socket):
+    while True:
+        if(number > 0 and number < 51):
+            return number
+        else:
+            send_message(client_socket, 'You must enter a number between 1 and 50')
+
+
 
 def start_game_between_player_and_computer():
     print('Game is starting between player and computer...')
+
     client_socket = clients_sockets[0]
-    
-    send_message(client_socket, 'Computer is choosing a number between 1 and 50...')
+    nr_of_tries = 0
+
+    send_message(client_socket, 'Computer is choosing a number between 1 and 50...\nComputer has chosen a number. Try to guess it...')
     random_number = random.randint(1, 50)
-    sleep(1)
-    send_message(client_socket, 'Computer has chosen a number. Try to guess it...')
 
     while True:
         message = client_socket.recv(1024).decode('utf-8')
-        number = int(message)
-
-        if message == 'EXIT':
-            break
+        number = handle_message(message, client_socket)
+        nr_of_tries += 1
 
         if(number == random_number):
             send_message(client_socket, 'You have guessed the number.\nDo you want to play again? (Y/N)')
+            
             message = client_socket.recv(1024).decode('utf-8')
-
-            if(message == 'Y'):
-                start_game_between_player_and_computer()
-            else:
-                send_score()
-            break
+            handle_confirm_answer(message, client_socket, nr_of_tries)
+                
         elif(number > random_number):
             send_message(client_socket, 'The number is lower than the one you have chosen.')
 
